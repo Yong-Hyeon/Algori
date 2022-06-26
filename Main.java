a7460a51558d3dbbafe0b6c10e225b6c37f52861
지용현 <yung5487@gmail.com>
2022-06-26 16:48:50 +0900

    백준12865 평범한배ㄷ낭

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int[] W = new int[N];
+		int[] V = new int[N];
+		int[] bag = new int[K + 1];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			W[i] = Integer.parseInt(st.nextToken());
+			V[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = K; j >= W[i]; j--) {
+				bag[j] = Math.max(bag[j], bag[j - W[i]] + V[i]);
+			}
+		}
+
+		System.out.println(bag[K]);
+
+	}
+
+}
