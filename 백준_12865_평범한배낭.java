645c3cbb5231042e554b84173be37bb1ca22e331
지용현 <yung5487@gmail.com>
2022-06-26 17:02:29 +0900

    백준 12865 평범한배낭

--- 백준_12865_평범한배낭.java
+++ 백준_12865_평범한배낭.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 백준_12865_평범한배낭 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
