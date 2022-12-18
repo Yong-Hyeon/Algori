df8d8cfdac0a2a9d88daa42608e9bd3663d58fda
지용현 <yung5487@gmail.com>
2022-12-18 11:14:35 +0900

    백준 1009 A/B

---
+++ 백준_1008_AdivB/Main.java
@@ -0,0 +1,17 @@
+package 백준_1008_AdivB;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+
+        System.out.println(A/B);
+    }
+}
