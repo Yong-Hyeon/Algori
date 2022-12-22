bd3a21d835e8358def7ab6de1ef71384d60e1ae5
지용현 <yung5487@gmail.com>
2022-12-23 00:01:00 +0900

    백준 10818 최소, 최대

---
+++ 백준_10818_최소최대/Main.java
@@ -0,0 +1,26 @@
+package 백준_10818_최소최대;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
