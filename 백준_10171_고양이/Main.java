8c11727d4d32848982b96cdc454229ecb48591a1
지용현 <yung5487@gmail.com>
2022-12-22 23:40:41 +0900

    백준 10171 고양이

---
+++ 백준_10171_고양이/Main.java
@@ -0,0 +1,16 @@
+package 백준_10171_고양이;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\\    /\\").append("\n");
+        sb.append(" )  ( ')").append("\n");
+        sb.append("(  /  )").append("\n");
+        sb.append(" \\(__)|");
+        System.out.println(sb);
+    }
+}
