fec080710fb8f3385c08f104080fbc9d37f5c885
지용현 <yung5487@gmail.com>
2022-12-23 00:03:05 +0900

    백준 10869 사칙연산

---
+++ 백준_10869_사칙연산/Main.java
@@ -0,0 +1,21 @@
+package 백준_10869_사칙연산;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
