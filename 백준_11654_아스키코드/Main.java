307c99b43ed556a3a70016672be938308e97b774
지용현 <yung5487@gmail.com>
2022-12-23 15:45:11 +0900

    백준 11654 아스키 코드

---
+++ 백준_11654_아스키코드/Main.java
@@ -0,0 +1,14 @@
+package 백준_11654_아스키코드;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char input = br.readLine().toCharArray()[0];
+
+        System.out.println((int)input);
+    }
+}
