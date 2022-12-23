788fe3ab09cdc7a838d2c030a8157c63eeb08923
지용현 <yung5487@gmail.com>
2022-12-23 15:20:20 +0900

    백준 10950 A + B - 3

---
+++ 백준_10950_AplusB3/Main.java
@@ -0,0 +1,24 @@
+package 백준_10950_AplusB3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 1; tc <= T; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append(A+B).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
