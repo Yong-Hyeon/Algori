362b60d5eca76723b419922a5d7ddf077d768f50
지용현 <yung5487@gmail.com>
2023-03-13 22:18:08 +0900

    백준 1261 알고스팟

--- 백준_1261_알고스팟/Main.java
+++ 백준_1261_알고스팟/Main.java
@@ -3,11 +3,28 @@ package 백준_1261_알고스팟;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static class Algo implements Comparable<Algo>{
+        int r,c,cnt;
+
+        Algo(int r, int c, int cnt){
+            this.r = r;
+            this.c = c;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public int compareTo(Algo o) {
+            return this.cnt - o.cnt;
+        }
+    }
     static int result;
+    static int[][] delta = {{1,0},{0,1},{-1,0},{0,-1}};
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,17 +38,32 @@ public class Main {
                 map[i][j] = Integer.parseInt(str[j]);
             }
         }
+        PriorityQueue<Algo> pq = new PriorityQueue<>();
+        boolean[][] isVisited = new boolean[N][M];
 
+        pq.add(new Algo(0,0,0));
+        isVisited[0][0] = true;
+        int result = 0;
+        while (!pq.isEmpty()){
+            Algo now = pq.poll();
 
+            if(now.r == N -1 && now.c == M - 1){
+                result = now.cnt;
+                break;
+            }
 
+            for(int i = 0; i < 4; i++){
+                int nr = now.r + delta[i][0];
+                int nc = now.c + delta[i][1];
+
+                if(nr < 0 || nr >= N || nc < 0 || nc >= M || isVisited[nr][nc]){
+                    continue;
+                }
+                pq.add(new Algo(nr,nc,now.cnt + map[nr][nc]));
+                isVisited[nr][nc] = true;
+            }
+        }
         System.out.println(result);
     }
 
-    /*
-    방문체크해서 방문 했으면 안간다. 근데 방문했어도 지금장소 + 다음장소 값이 다음장소에 저장된 값보다 작으면 간다.
-    dp[now] + 1 < dp[next] 같은 느낌으로.
-     */
-    static void dfs(int N, int M, int r, int c){
-
-    }
 }
