f66b0897066579566502ed7b5f8e4a138b447df3
8gi_coach@ssafy.com <8gi_coach@ssafy.com>
2023-04-13 13:52:44 +0900

    백준 7490 0 만들기

--- 백준_7490_0만들기/Main.java
+++ 백준_7490_0만들기/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -16,29 +15,68 @@ public class Main {
         for (int tc = 1; tc <= T; tc++) {
             int N = Integer.parseInt(br.readLine());
             char[] combis = new char[N * 2 - 1];
-            combi(N, opers, combis, 0, 1);
-
+            combi(N, opers, combis, 0, 1, sb);
 
             sb.append("\n");
         }
 
         System.out.println(sb);
     }
-
-    private static void combi(int N, char[] opers, char[] combis, int cnt, int start) {
+    private static void combi(int N, char[] opers, char[] combis, int cnt, int numCnt, StringBuilder sb) {
         if (cnt == N * 2 - 1) {
             // 계산하기
+            Queue<String> queue = new LinkedList<>();
+            String temp = "" + (int)combis[0];
+            for(int i = 1; i < cnt; i++){
+                if(combis[i] == ' '){
+                    continue;
+                }else if(combis[i] == '+'){
+                    queue.add(temp);
+                    temp = "";
+                    queue.add(combis[i] + "");
+                }else if(combis[i] == '-'){
+                    queue.add(temp);
+                    temp = "";
+                    queue.add(combis[i] + "");
+                }else{
+                    temp += (int)combis[i];
+                }
+            }
+            queue.add(temp);
+
+            int sum = Integer.parseInt(queue.poll());
+
+            while (!queue.isEmpty()){
+                String str = queue.poll();
+                if(str.equals("+")){
+                    sum += Integer.parseInt(queue.poll());
+                }else if(str.equals("-")){
+                    sum -= Integer.parseInt(queue.poll());
+                }
+            }
+
+            if(sum == 0){
+                for(int i = 0; i < cnt; i++){
+                    if(combis[i] == ' ' || combis[i] == '+' || combis[i] == '-'){
+                        sb.append(combis[i]);
+                    }else{
+                        sb.append((int)combis[i]);
+                    }
+                }
+                sb.append("\n");
+            }
+            return;
         }
 
-        for (int i = start; i < 10; i++) {
-            if (cnt % 2 == 0) {
-                opers[cnt] = (char)i;
-            } else {
-                for (int k = 0; k < 3; k++) {
-
-                }
-
+        if (cnt % 2 == 0) {
+            combis[cnt] = (char)numCnt;
+            combi(N, opers, combis, cnt + 1, numCnt + 1, sb);
+        } else {
+            for (int k = 0; k < 3; k++) {
+                combis[cnt] = opers[k];
+                combi(N, opers, combis, cnt + 1, numCnt, sb);
             }
+
         }
 
     }
