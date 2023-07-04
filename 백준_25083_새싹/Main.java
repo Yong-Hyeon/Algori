3366c82b1cafdc9f1d942b38b4844f8f284d1a63
jiyonghyeon <yung5487@gmail.com>
2023-07-05 08:51:24 +0900

    백준 25083 새싹

---
+++ 백준_25083_새싹/Main.java
@@ -0,0 +1,14 @@
+package 백준_25083_새싹;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+
+}
