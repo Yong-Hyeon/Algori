dc20539b9c2ea8b3149ef166c16bdbd968a0436d
지용현 <yung5487@gmail.com>
2023-07-19 23:19:03 +0900

    백준 2164 카드2

---
+++ 백준_2164_카드2/Main.java
@@ -0,0 +1,29 @@
+package 백준_2164_카드2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 1; i <= N; i++){
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()){
+            if(queue.size() == 1){
+                break;
+            }
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
