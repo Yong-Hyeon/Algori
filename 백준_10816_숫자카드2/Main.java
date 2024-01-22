631357ec9ed9572857ce46118a3581568a97e521
지용현 <yung5487@gmail.com>
2024-01-22 22:00:02 +0900

    백준 10816 숫자 카드 2 Map에서 배열로 코드 변경

--- 백준_10816_숫자카드2/Main.java
+++ 백준_10816_숫자카드2/Main.java
@@ -2,8 +2,6 @@ package 백준_10816_숫자카드2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,26 +10,18 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = null;
         int N = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] arr = new int[20000001];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
             int num = Integer.parseInt(st.nextToken());
-            if(map.containsKey(num)){
-                map.put(num, map.get(num)+1);
-            }else{
-                map.put(num,1);
-            }
+            arr[num+10000000]++;
         }
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++){
             int num = Integer.parseInt(st.nextToken());
-            if(map.containsKey(num)){
-                sb.append(map.get(num)).append(" ");
-            }else{
-                sb.append(0).append(" ");
-            }
+            sb.append(arr[num+10000000]).append(" ");
         }
 
         System.out.println(sb);
