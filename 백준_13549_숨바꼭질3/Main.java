92a4bd788f7a176c880276de2515e090833246fc
지용현 <yung5487@gmail.com>
2024-07-08 22:11:43 +0900

    백준 13549 숨바꼭질 3

--- 백준_13549_숨바꼭질3/Main.java
+++ 백준_13549_숨바꼭질3/Main.java
@@ -7,70 +7,78 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Point {
-        int time;
-        int point;
-
-        Point(int point, int time) {
-            this.time = time;
-            this.point = point;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int cnt = 0;
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        boolean[] isVisited = new boolean[200001];
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(N, 0));
-
-        if(N > K){
-            cnt = N - K;
-            queue.poll();
+        boolean[] isVisited = new boolean[100001];
+        int time = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        if (N >= K){
+            time = N - K;
+        }else{
+            queue.add(N);
+            isVisited[N] = true;
         }
+        boolean isEnd = false;
 
-        while (!queue.isEmpty()) {
-
-
-            Point current = queue.poll();
-
-            if (current.point == K) {
-                cnt = current.time;
-                break;
-            }
-
-            for (int j = 0; j < 3; j++) {
-                int next = 0;
-                if (j == 2) {
-                    next = current.point - 1;
-                    if (next >= 0 && !isVisited[next]) {
-                        queue.add(new Point(next, current.time + 1));
-//                            isVisited[next] = true;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int num = queue.poll();
+                queue.add(num);
+                num = num * 2;
+                while (num < 100001){
+                    if(num == K){
+                        isEnd = true;
+                        break;
                     }
-                } else if (j == 1) {
-                    next = current.point + 1;
-                    if (next <= 200000 && !isVisited[next]) {
-                        queue.add(new Point(next, current.time + 1));
-//                            isVisited[next] = true;
+                    if(isVisited[num]){
+                        break;
                     }
-                } else if (j == 0) {
-                    for (int k = current.point; k <= 200000; k *= 2) {
-                        if (isVisited[k]) {
-                            break;
-                        }
-                        if (!isVisited[k]) {
-                            queue.add(new Point(k, current.time));
-                            isVisited[k] = true;
-                        }
-                    }
+                    isVisited[num] = true;
+                    queue.add(num);
+                    num = num * 2;
+                }
+                if(isEnd){
+                    break;
                 }
             }
+            if(isEnd){
+                break;
+            }
+            size = queue.size();
+            time++;
+            for(int i = 0; i < size; i++){
+                int now = queue.poll();
+
+                int next = now + 1;
+                if(next == K){
+                    isEnd = true;
+                    break;
+                }
+                if(next < 100001 && !isVisited[next]){
+                    isVisited[next] = true;
+                    queue.add(next);
+                }
+
+                next = now - 1;
+                if(next == K){
+                    isEnd = true;
+                    break;
+                }
+                if(next >= 0 && !isVisited[next]){
+                    isVisited[next] = true;
+                    queue.add(next);
+                }
+            }
+            if(isEnd){
+                break;
+            }
         }
 
 
-        System.out.println(cnt);
+        System.out.println(time);
     }
 }
