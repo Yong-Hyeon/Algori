c5ccd278434619c66b0866db408bdc4ab6efaeaa
지용현 <yung5487@gmail.com>
2024-07-10 02:48:53 +0900

    백준 15666 N과 M (12)

---
+++ 백준_15663_N과M12/Main.java
@@ -0,0 +1,56 @@
+package 백준_15663_N과M12;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] input = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Map<String, Boolean> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        Arrays.sort(input);
+
+        combi(input, N, M, new int[M], 0, map, list);
+
+        for(int i = 0; i < list.size(); i++){
+            sb.append(list.get(i)).append("\n");
+        }
+
+
+        System.out.println(sb);
+    }
+
+    private static void combi(int[] input, int N, int M, int[] select, int cnt, Map<String, Boolean> map, List<String> list) {
+        if(cnt == M){
+            String str = "";
+            for(int i = 0; i < M; i++){
+                str = str + input[select[i]] + " ";
+            }
+            if(!map.containsKey(str)) {
+                map.put(str, true);
+                list.add(str);
+            }
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(cnt > 0 && select[cnt-1] > i){
+                continue;
+            }
+            select[cnt] = i;
+            combi(input, N, M, select, cnt + 1, map, list);
+        }
+    }
+
+}
