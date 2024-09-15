e49d15fa8ab4c38f42d25ef639a99ae021a9f7a4
지용현 <yung5487@gmail.com>
2024-09-16 00:12:52 +0900

    백준 8393 합

---
+++ 백준_8393_합/Main.java
@@ -0,0 +1,3 @@
+package 백준_8393_합;
+
+import java.util.*;public class Main {public static void main(String[] args) throws Exception{Scanner s=new Scanner(System.in);int n=s.nextInt();System.out.println(n*(n+1)/2);}}
