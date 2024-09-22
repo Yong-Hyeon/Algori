5229c946551f6f98d0fe2e057544245dd629d75b
지용현 <yung5487@gmail.com>
2024-09-22 22:15:27 +0900

    백준 5338 마이크로소프트 로고

---
+++ 백준_5338_마이크로소프트로고/Main.java
@@ -0,0 +1,3 @@
+package 백준_5338_마이크로소프트로고;
+
+interface Main{static void main(String[]a){System.out.print("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");}}
