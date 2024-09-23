c57ed4d4079ce8fb757ba594a738cb683323feae
지용현 <yung5487@gmail.com>
2024-09-23 23:05:18 +0900

    백준 5339 콜센터

---
+++ 백준_5339_콜센터/Main.java
@@ -0,0 +1,3 @@
+package 백준_5339_콜센터;
+
+interface Main{static void main(String[]a){System.out.print("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |");}}
