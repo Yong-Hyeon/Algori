c3a741d5f9586a438226c213e6381f394710b386
지용현 <yung5487@gmail.com>
2024-09-26 00:17:30 +0900

    백준 7891 Can you add this?

---
+++ 백준_7891_Canyouaddthis/Main.java
@@ -0,0 +1,21 @@
+package 백준_7891_Canyouaddthis;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
