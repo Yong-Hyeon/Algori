4951fbc0f943c3a6c78c04fd0eed035b83508ed4
지용현 <yung5487@gmail.com>
2024-09-28 14:39:46 +0900

    백준 9654 나부 함대 데이터

---
+++ 백준_9654_나부함대데이터/Main.java
@@ -0,0 +1,3 @@
+package 백준_9654_나부함대데이터;
+
+interface Main{static void main(String[]a){System.out.print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21        \nJ-Type 327     Light Combat   Unlimited  1         \nNX Cruiser     Medium Fighter Limited    18        \nN1 Starfighter Medium Fighter Unlimited  25        \nRoyal Cruiser  Light Combat   Limited    4         ");}}
