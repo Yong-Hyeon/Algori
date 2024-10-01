2f7928816cef1a85971b41235bdaac0e7953383d
지용현 <yung5487@gmail.com>
2024-10-01 20:45:18 +0900

    백준 16430 제리와 톰

---
+++ 백준_16430_제리와톰/Main.java
@@ -0,0 +1,20 @@
+package 백준_16430_제리와톰;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+
+        sb.append(B-A).append(" ").append(B);
+        System.out.println(sb);
+    }
+}
