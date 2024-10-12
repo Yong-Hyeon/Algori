e65120edf420c07d07113730daf153f227b2a612
지용현 <yung5487@gmail.com>
2024-10-12 23:27:02 +0900

    백준 14581 팬들에게 둘러싸인 홍준

---
+++ 백준_14581_팬들에게둘러싸인홍준/Main.java
@@ -0,0 +1,3 @@
+package 백준_14581_팬들에게둘러싸인홍준;
+
+interface Main{static void main(String[]a){System.out.print(":fan::fan::fan:\n:fan::"+new java.util.Scanner(System.in).next()+"::fan:\n:fan::fan::fan:");}}
