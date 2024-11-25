5b9c86da0ff1fdac181ac04610f1cfe12f8db851
지용현 <yung5487@gmail.com>
2024-11-25 17:06:07 +0900

    백준 26711 A+B

---
+++ 백준_26711_APlusB/Main.java
@@ -0,0 +1,3 @@
+package 백준_26711_APlusB;
+
+import java.util.*;interface Main{static void main(String[]a){Scanner c=new Scanner(System.in);System.out.print(c.nextBigInteger().add(c.nextBigInteger()));}}
