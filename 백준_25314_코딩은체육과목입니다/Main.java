8b46ec1a817d9385e6f36f2450612cb13ff9c154
지용현 <yung5487@gmail.com>
2024-11-26 23:49:53 +0900

    백준 25314 코딩은 체육과목 입니다

---
+++ 백준_25314_코딩은체육과목입니다/Main.java
@@ -0,0 +1,3 @@
+package 백준_25314_코딩은체육과목입니다;
+
+interface Main{static void main(String[]a){int n=new java.util.Scanner(System.in).nextInt()/4;while(n-->0){System.out.print("long ");}System.out.print("int");}}
